3b9fda9f1bab63ed4998c232d59d37ac0d4f6aca
MikaCaldera <mcaldera@mirandacreative.com>
2018-02-20 15:04:40 -0500

    Removed some extra scripts that were not being used

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
                 <div class="row">
                     <div class="small-12 medium-8 column">
                         <div class="footer__bottom-left">
-                            <p class="copyright">Copyright March Farm 2017</p>
+                            <p class="copyright">Copyright March Farm 2018</p>
                         </div>
                     </div>
                     <div class="small-12 medium-4 column">
@@ -36,41 +36,20 @@
             </footer><!-- END of footer -->
             <link href='wp-content/plugins/wd-instagram-feed/css/wdi_frontend.min-ver=1.2.15.css' id='wdi_frontend_thumbnails-css' media='all' rel='stylesheet' type='text/css'>
             <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?ver=1.2.15' id='font-awesome-css' media='all' rel='stylesheet' type='text/css'>
-            <!--MR 2-20-2018-->
-            <script src="./js/jQuery_3_slim_min.js">
-            </script>
-            <script type="text/javascript" src="./js/script.js"></script>
-            <!--/MR-->
+            <!-- <script src="./js/jQuery_3_slim_min.js"></script>
+            <script type="text/javascript" src="./js/script.js"></script> -->
             <script src='wp-content/themes/march-farm/js/foundation.min-ver=6.3.0.js' type='text/javascript'></script>
             <script src='wp-content/themes/march-farm/js/plugins/slick.min-ver=1.6.0.js' type='text/javascript'></script>
             <script src='wp-content/themes/march-farm/js/plugins/jquery.matchHeight-min-ver=0.7.0.js' type='text/javascript'></script>
             <script src='wp-content/themes/march-farm/js/plugins/jquery.fancybox.pack-ver=2.1.5.js' type='text/javascript'></script>
-            <!--MR  <script src='wp-content/themes/march-farm/js/global.js' type='text/javascript'></script> /MR-->
+            <script src='wp-content/themes/march-farm/js/global.js' type='text/javascript'></script>
             <script src='wp-includes/js/wp-embed.min-ver=4.9.4.js' type='text/javascript'></script>
             <script src='wp-includes/js/underscore.min-ver=1.8.3.js' type='text/javascript'>  </script>
-            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_instagram.min-ver=1.2.15.js' type='text/javascript'>     </script>
-            <script type='text/javascript'>
-            /* <![CDATA[ */
-            var wdi_ajax = {"ajax_url":"http:\/\/marchfarmoutings.com\/wp-admin\/admin-ajax.php"};
-            var wdi_url = {"plugin_url":"http:\/\/marchfarmoutings.com\/wp-content\/plugins\/wd-instagram-feed\/","ajax_url":"http:\/\/marchfarmoutings.com\/wp-admin\/admin-ajax.php"};
-            var wdi_front_messages = {"connection_error":"Connection Error, try again later :(","user_not_found":"Username not found","network_error":"Network error, please try again later :(","hashtag_nodata":"There is no data for that hashtag","filter_title":"Click to filter images by this user","invalid_users_format":"Provided feed users are invalid or obsolete for this version of plugin","feed_nomedia":"There is no media in this feed","follow":"Follow","show_alerts":"","wdi_token_flag_nonce":"7b338fb85e","wdi_token_error_flag":""};
-            var wdi_feed_0 = {"feed_row":{"thumb_user":"marchfarm","feed_name":"Sample Feed","feed_thumb":"https:\/\/scontent.cdninstagram.com\/t51.2885-19\/s150x150\/20837391_461262784272318_5464830002753175552_a.jpg","published":"1","theme_id":"1","feed_users":"[{\"username\":\"marchfarm\",\"id\":\"5862355872\"}]","feed_display_view":"none","sort_images_by":"date","display_order":"desc","follow_on_instagram_btn":"1","display_header":"0","number_of_photos":"3","load_more_number":"3","pagination_per_page_number":"12","pagination_preload_number":"10","image_browser_preload_number":"10","image_browser_load_number":"10","number_of_columns":"3","resort_after_load_more":"0","show_likes":"0","show_description":"0","show_comments":"0","show_usernames":"0","display_user_info":"0","display_user_post_follow_number":"0","show_full_description":"1","disable_mobile_layout":"0","feed_type":"thumbnails","feed_item_onclick":"lightbox","popup_fullscreen":"0","popup_width":"640","popup_height":"640","popup_type":"none","popup_autoplay":"0","popup_interval":"5","popup_enable_filmstrip":"0","popup_filmstrip_height":"70","autohide_lightbox_navigation":"1","popup_enable_ctrl_btn":"1","popup_enable_fullscreen":"1","popup_enable_info":"0","popup_info_always_show":"0","popup_info_full_width":"0","popup_enable_comment":"0","popup_enable_fullsize_image":"1","popup_enable_download":"0","popup_enable_share_buttons":"0","popup_enable_facebook":"0","popup_enable_twitter":"0","popup_enable_google":"0","popup_enable_pinterest":"0","popup_enable_tumblr":"0","show_image_counts":"0","enable_loop":"1","popup_image_right_click":"1","conditional_filters":"","conditional_filter_type":"none","show_username_on_thumb":"0","conditional_filter_enable":"0","liked_feed":"userhash","mobile_breakpoint":"640","redirect_url":"","feed_resolution":"optimal","id":"1","widget":false,"access_token":"5862355872.54da896.a0d9288bf22f477bac0a8062af574c43","wdi_feed_counter":0},"data":[],"usersData":[],"dataCount":"0"};
-            var wdi_front = {"feed_counter":"0"};
-            /* ]]> */
-            </script>
-            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_frontend.min-ver=1.2.15.js' type='text/javascript'>
-            </script>
-            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_responsive.min-ver=1.2.15.js' type='text/javascript'>
-            </script>
-            <script src='wp-content/plugins/wd-instagram-feed/js/gallerybox/jquery.mobile-ver=1.2.15.js' type='text/javascript'>
-            </script>
-            <script src='wp-content/plugins/wd-instagram-feed/js/gallerybox/jquery.fullscreen-0.4.1-ver=1.2.15.js' type='text/javascript'>
-            </script>
-            <script type='text/javascript'>
-            /* <![CDATA[ */
-            var wdi_objectL10n = {"wdi_field_required":"Field is required.","wdi_mail_validation":"This is not a valid email address.","wdi_search_result":"There are no images matching your search."};
-            /* ]]> */
-            </script>
+            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_instagram.min-ver=1.2.15.js' type='text/javascript'></script>
+            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_frontend.min-ver=1.2.15.js' type='text/javascript'></script>
+            <script src='wp-content/plugins/wd-instagram-feed/js/wdi_responsive.min-ver=1.2.15.js' type='text/javascript'></script>
+            <script src='wp-content/plugins/wd-instagram-feed/js/gallerybox/jquery.mobile-ver=1.2.15.js' type='text/javascript'></script>
+            <script src='wp-content/plugins/wd-instagram-feed/js/gallerybox/jquery.fullscreen-0.4.1-ver=1.2.15.js' type='text/javascript'></script>
             <script src='wp-content/plugins/wd-instagram-feed/js/gallerybox/wdi_gallery_box.min-ver=1.2.15.js' type='text/javascript'>
             </script>
             <script>
